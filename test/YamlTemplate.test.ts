74b76a83b5fb0202d0e078f7d6453435e77aea78
dusan <dusan@rezdy.com>
2019-01-24 10:56:45 +1100

    new features

--- test/YamlTemplate.test.ts
+++ test/YamlTemplate.test.ts
@@ -1,44 +1,50 @@
-import {YamlTemplate} from "../src";
+import {load, YamlTemplate} from "../src";
 import {dirname, join} from 'path'
 import {readFileSync} from "fs";
 
 describe("Template resolving", () => {
 
-    let template: YamlTemplate;
-
-    beforeAll(() => template = new YamlTemplate());
-
 
     it("load the file", () => {
-        let resolved = template.loadFile(join(__dirname, 'serverless/simple.core.yml'));
+        let resolved = load(join(__dirname, 'serverless/simple.core.yml'));
         expect(resolved).toBeDefined();
+    });
+
+    it("should replace multiple vars at the same line", async () => {
+
+        let resolved = new YamlTemplate().replaceVars(
+            `name: \${opt:nonExistingParam}-\${self:custom.name}`,
+            new Map([['custom.name', 'foo']]));
+
+        expect(resolved).toBe(`name: \${opt:nonExistingParam}-foo`);
     });
 
     it("should replace parametrized file with content", async () => {
 
         const filePath = join(__dirname, 'serverless/simple.core.yml');
-        let resolved = template.replaceFiles(readFileSync(filePath, 'utf8'), dirname(filePath));
+        let resolved = new YamlTemplate().replaceFiles(readFileSync(filePath, 'utf8'), dirname(filePath));
         expect(resolved).toBe(
 `service: webhookService
-  provider:
-    name: \${custom:name}
-    stage: \${opt:stage}
-    env: \${opt:stage}
+provider:
+  name: \${opt:nonExistingParam}-\${self:custom.name}
+  stage: \${opt:stage}
+  env: \${opt:stage}
   environment:
-    ENV: test`);
+    ENV: test
+  foo: bar`);
 
         console.log(resolved);
     });
 
     it("should replace custom and opt", async () => {
 
-        let resolved = template.replaceVars(
+        let resolved = new YamlTemplate().replaceVars(
             `service: webhookService
                 provider:
-                  name: \${custom:name}
+                  name: \${self:custom.name}
                   stage: \${opt:stage}
                   env: \${opt:stage}`,
-            new Map([['name', 'foo'], ['stage', 'test']]));
+            new Map([['custom.name', 'foo'], ['stage', 'test']]));
 
         expect(resolved).toBe(
             `service: webhookService
@@ -48,20 +54,21 @@ describe("Template resolving", () => {
                   env: test`);
     });
 
-    it("should replace file and custom and opt variables", async () => {
+    it("should replace file and self and opt variables", async () => {
 
         const filePath = join(__dirname, 'serverless/simple.core.yml');
-        let resolved = template.loadFile(filePath,
-            new Map([['name', 'foo'], ['stage', 'test']]));
+        let resolved = new YamlTemplate().loadFile(filePath,
+            new Map([['custom.name', 'foo'], ['stage', 'test']]));
 
         expect(resolved).toBe(
             `service: webhookService
-  provider:
-    name: foo
-    stage: test
-    env: test
+provider:
+  name: \${opt:nonExistingParam}-foo
+  stage: test
+  env: test
   environment:
-    ENV: test`);
+    ENV: test
+  foo: bar`);
     });
 });
 
